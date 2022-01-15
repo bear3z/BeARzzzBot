ff1bfe5696ebadc0d47de7c45a85e702e34da88f
eth3lbert <eth3lbert+dev@gmail.com>
2022-01-15 18:34:28 +0800

    chore: introduce orjson

--- src/bearzzzbot/routes.py
+++ src/bearzzzbot/routes.py
@@ -1,8 +1,9 @@
+from fastapi.responses import ORJSONResponse
 from fastapi.routing import APIRouter
 
 from bearzzzbot.drawings.resources import router as drawing_router
 
-api_router = APIRouter()
+api_router = APIRouter(default_response_class=ORJSONResponse)
 
 
 api_router.include_router(drawing_router, prefix="/drawing")
