dee705c58970884620c74792d372a6f1bd93600a
eth3lbert <eth3lbert+dev@gmail.com>
2022-01-15 15:00:08 +0800

    refactor: move app callable to asgi

--- src/main.py
+++ src/main.py
@@ -5,33 +5,36 @@ from fastapi.templating import Jinja2Templates
 from .config import Settings, get_settings
 from .insert_db import insert_db
 
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
 
+def make_app() -> FastAPI:
+    settings = get_settings()
+    app = FastAPI(title=settings.APP_NAME)
+    templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def home_get(request: Request, settings: Settings = Depends(get_settings)):
-    return templates.TemplateResponse(
-        "main.html",
-        {
-            "request": request,
-            "liffID": settings.LIFFID,
-        },
-    )
+    @app.get("/", response_class=HTMLResponse)
+    async def home_get(request: Request, settings: Settings = Depends(get_settings)):
+        return templates.TemplateResponse(
+            "main.html",
+            {
+                "request": request,
+                "liffID": settings.LIFFID,
+            },
+        )
 
+    @app.get("/data/{statement}")
+    async def send_data_to_db(
+        statement: str,
+    ):
+        stm = statement.split("&")
+        data = []
+        data.append(stm[0])
 
-@app.get("/data/{statement}")
-async def send_data_to_db(
-    statement: str,
-):
-    stm = statement.split("&")
-    data = []
-    data.append(stm[0])
+        for i in range(1, 6):
+            if stm[i] == "true":
+                data.append(1)
+            else:
+                data.append(0)
+        insert_db(data)
+        return "OK"
 
-    for i in range(1, 6):
-        if stm[i] == "true":
-            data.append(1)
-        else:
-            data.append(0)
-    insert_db(data)
-    return "OK"
+    return app
